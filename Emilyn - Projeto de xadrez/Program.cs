8bc95f0f1aaa1c500087a02c9a54f48ff942865e
Emilyn Bernardes <emilynlinobernardes@gmail.com>
2022-02-08 17:33:25 -0300

    Controlando as pecas capturadas

--- Emilyn - Projeto de xadrez/Program.cs
+++ Emilyn - Projeto de xadrez/Program.cs
@@ -19,10 +19,7 @@ namespace Emilyn___Projeto_de_xadrez
                     try
                     {
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                        Tela.imprimirPartida(partida);
 
                         Console.WriteLine();
                         Console.Write("Origem: ");
